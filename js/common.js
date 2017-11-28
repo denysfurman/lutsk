0c6cacaa26e5e57840c42e20da1ea44d093e16f4
denys furman <furik1102@gmail.com>
2017-11-28 19:50:43 +0200

    all

--- js/common.js
+++ js/common.js
@@ -1,6 +1,5 @@
-$(function() {
-   	
-   	// loader
+
+// loader
 	$(document).ready(function() {
 	  setTimeout(function() {
 	    $('body').addClass('loaded');
@@ -10,19 +9,45 @@ $(function() {
 
 	});
 
+$(function() {
+   	
+   	
+
 	// animated
 
 	$('.service_img.left_part').animated('bounceInLeft' , 'bounceInLeft' );
 	$('.service_img.right_part').animated('bounceInRight' , 'bounceInRight' );
 	$('.service_text h1').animated('zoomIn' , 'zoomIn' );
 	
-
+	//slider mobile
+		$('.mobile_item_slider').owlCarousel({
+		
+		    loop:true,
+		    margin:0,
+		   	nav:true,
+		   
+		    
+		    navText:[
+	            "<i class='fa fa-angle-left fa-2x'></i>",
+	            "<i class='fa fa-angle-right fa-2x'></i>"
+	        ],
+	        items:1
+		  
+		});	
 	//SVG Fallback
 	if(!Modernizr.svg) {
 		$("img[src*='svg']").attr("src", function() {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
 	};
+
+	//togle button
+
+	$('.menu-toggle').on('click',function(){
+		$('.menu-toggle').toggleClass('active');
+			
+		$(".heade_mob_menu").slideToggle();
+	});
 
 	
 
